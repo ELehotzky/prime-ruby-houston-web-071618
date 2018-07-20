f79de3c7c273321e9ea7423cad20e8b699f57859
Erica Lehotzky <lehotzky.erica@gmail.com>
2018-07-20 11:35:47 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,9 @@
 # Add  code here!
+
+    def is_prime?
+        return false if self <= 1
+        2.upto(Math.sqrt(self).to_i) do |x|
+            return false if self%x == 0
+        end 
+        true
+    end 
